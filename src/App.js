022738b5b2bd44c6d38c1e63a1e2cdcace882750
Lucas <ra1957066@gmail.com>
2020-11-26 23:37:07 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [gotResponse, setGotResponse] = useState(false);
+  const locale = navigator.language === 'pt-BR' ? 'pt-BR' : 'en-US';
+  const localString = {
+    'en-US': 'Waiting for Heroku response...',
+    'pt-BR': 'Aguardando resposta da Heroku...'
+  };
+  
+  useEffect(() => {
+    if (!gotResponse) {
+      axios.get('https://odinclone.herokuapp.com/api/islogged')
+        .then((response) => {
+          setGotResponse(true);
+        })
+        .catch((err) => {
+          setGotResponse(true);
+        })
+    } else {
+      if (locale === 'en-US') {
+        window.location.href = 'https://odinclone.herokuapp.com/en';
+      } else {
+        window.location.href = 'https://odinclone.herokuapp.com';
+      }
+    }
+  }, [gotResponse, locale])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="loading-screen">
+        <div>
+          <i className="fas fa-spinner loading-spinner"></i>
+        </div>
+          { localString[locale] }
+        </div>
     </div>
   );
 }
